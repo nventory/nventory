2e0bd16088e81ad5f759711137df74e6de4aac7e
jheiss <jheiss@ee601400-a34b-0410-bf97-8c122c3a034e>
2008-08-15 21:53:04 +0000

    Move the code which creates the standard status values here from the 019 migration, which is going away.

--- server/trunk/db/migrate/010_create_statuses.rb
+++ server/trunk/db/migrate/010_create_statuses.rb
@@ -15,6 +15,27 @@ class CreateStatuses < ActiveRecord::Migration
     add_column "nodes", "status_id", :integer
     add_index :nodes, :status_id
 
+    # Some System Install Defaults
+    s1 = Status.new
+    s1.name = 'inservice'
+    s1.notes = 'Hardware and OS functional, applications running'
+    s1.save
+    s2 = Status.new
+    s2.name = 'outofservice'
+    s2.notes = 'Hardware and OS functional, applications not running'
+    s2.save
+    s3 = Status.new
+    s3.name = 'available'
+    s3.notes = 'Hardware functional, no applications assigned'
+    s3.save
+    s4 = Status.new
+    s4.name = 'broken'
+    s4.notes = 'Hardware or OS not functional'
+    s4.save
+    s5 = Status.new
+    s5.name = 'setup'
+    s5.notes = 'New node, not yet configured'
+    s5.save
   end
 
   def self.down
