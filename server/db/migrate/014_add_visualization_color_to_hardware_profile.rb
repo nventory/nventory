816270b981c47273583f450fa2c25bb194971203
John Tran <jhtran@att.com>
2010-11-22 21:35:31 +0000

    submitting a tag to increment the client due to new cli release, without having to release corresponding server

--- server/db/migrate/014_add_visualization_color_to_hardware_profile.rb
+++
@@ -1,9 +0,0 @@
-class AddVisualizationColorToHardwareProfile < ActiveRecord::Migration
-  def self.up
-    add_column "hardware_profiles", "visualization_color", :string, :default => 'red'
-  end
-
-  def self.down
-    remove_column "hardware_profiles", "visualization_color"
-  end
-end
