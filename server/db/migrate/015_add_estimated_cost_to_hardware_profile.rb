64dbbf0c0db148cb9543d5bf80156527894abfac
John Tran <jhtran@att.com>
2010-12-22 18:21:04 +0000

    added some tag helpers to both perl&ruby api and cli.

--- server/db/migrate/015_add_estimated_cost_to_hardware_profile.rb
+++
@@ -1,8 +0,0 @@
-class AddEstimatedCostToHardwareProfile < ActiveRecord::Migration
-  def self.up
-    # Moved to #006
-  end
-
-  def self.down
-  end
-end
--- server/db/migrate/20100208234422_migrate_admin_accounts.rb
+++
@@ -1,9 +0,0 @@
-class MigrateAdminAccounts < ActiveRecord::Migration
-  def self.up
-    admins = Account.find(:all, :conditions => 'admin = 1')
-    admins.each{|acc| acc.authz.has_role 'admin'}
-  end
-
-  def self.down
-  end
-end
