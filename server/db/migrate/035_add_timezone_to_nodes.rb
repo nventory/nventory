ed338b3b7a916a9739706204fb38de895ce917d5
John Tran <jhtran@att.com>
2011-01-06 20:15:32 +0000

    cutting a new tag for possible release

--- server/db/migrate/035_add_timezone_to_nodes.rb
+++
@@ -1,9 +0,0 @@
-class AddTimezoneToNodes < ActiveRecord::Migration
-  def self.up
-    add_column :nodes, :timezone, :string
-  end
-
-  def self.down
-    remove_column :nodes, :timezone
-  end
-end
