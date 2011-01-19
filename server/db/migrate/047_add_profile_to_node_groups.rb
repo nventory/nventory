812476a6704aff894c9849be1266b3fea4f83e76
John Tran <jhtran@att.com>
2011-01-19 02:12:23 +0000

    Makefile changes for deb pkg, as per thepob checked in

--- server/db/migrate/047_add_profile_to_node_groups.rb
+++
@@ -1,9 +0,0 @@
-class AddProfileToNodeGroups < ActiveRecord::Migration
-  def self.up
-    add_column :node_groups, :lb_profile_id, :integer
-  end
-
-  def self.down
-    remove_column :node_groups, :lb_profile_id
-  end
-end
--- server/db/migrate/050_change_consumer_type_in_outlets.rb
+++
@@ -1,9 +0,0 @@
-class ChangeConsumerTypeInOutlets < ActiveRecord::Migration
-  def self.up
-    change_column "outlets", "consumer_type", :string, :default => 'Node'
-  end
-
-  def self.down
-    change_column "outlets", "consumer_type", :string
-  end
-end
