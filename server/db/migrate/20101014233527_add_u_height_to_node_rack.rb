9d24caf31fb08a00c74d726e9515caff961d6339
John Tran <jhtran@att.com>
2011-01-25 21:31:34 +0000

    no changes, but internally this tag is for release of new ca.pem with updated CA cert

--- server/db/migrate/20101014233527_add_u_height_to_node_rack.rb
+++
@@ -1,9 +0,0 @@
-class AddUHeightToNodeRack < ActiveRecord::Migration
-  def self.up
-    add_column 'node_racks', :u_height, :integer, :default => 42
-  end
-
-  def self.down
-    drop_column 'node_racks', :u_height
-  end
-end
