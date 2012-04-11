0c49c1c587f92144de24bc0fdb576b74b6e92018
thepob <thepob@ee601400-a34b-0410-bf97-8c122c3a034e>
2012-04-11 02:38:51 +0000

    getting rid of that annoying 'warning: peer certificate won't be verified in this SSL session' warning that the ruby client throws

--- trunk/client/ruby/nventory.rb
+++ trunk/client/ruby/nventory.rb
@@ -1174,6 +1174,7 @@ class NVentory::Client
         http.tmp_dh_callback = proc { dh }
       end
       http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
       if @ca_file && File.exist?(@ca_file)
         http.ca_file = @ca_file
         http.verify_mode = OpenSSL::SSL::VERIFY_PEER
